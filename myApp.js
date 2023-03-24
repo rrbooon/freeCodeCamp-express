868fa65a50e7cd25d574155da5d07f7feefd6b88
Rogério Santos <97257744+rrbooon@users.noreply.github.com>
2023-03-24 15:20:42 +0000

    feat: return a json in json route

--- myApp.js
+++ myApp.js
@@ -6,6 +6,12 @@ const publicPath = __dirname + '/public';
 
 app.use('/public', express.static(publicPath));
 
+app.get('/json', function (req, res){
+  res.json({
+    "message": "Hello json"
+  })
+});
+
 app.get('/', function(req, res) {
   res.sendFile(absolutePath);
 });
