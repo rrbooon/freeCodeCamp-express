cd5d586f3338b14716d84457d7249f56b9029b9a
Rogério Santos <97257744+rrbooon@users.noreply.github.com>
2023-03-24 15:17:39 +0000

    feat: add public paste in a middleware

--- myApp.js
+++ myApp.js
@@ -1,16 +1,14 @@
 let express = require('express');
 let app = express();
 
-const absolutePath = __dirname + '/views/index.html'
+const absolutePath = __dirname + '/views/index.html';
+const publicPath = __dirname + '/public';
 
-app.get('/', function (req, res) {
-  res.sendFile(absolutePath)
-})
+app.use('/public', express.static(publicPath));
 
-
-
-
-
+app.get('/', function(req, res) {
+  res.sendFile(absolutePath);
+});
 
 
 
@@ -40,4 +38,9 @@ app.get('/', function (req, res) {
 
 
 
- module.exports = app;
+
+
+
+
+
+module.exports = app;
