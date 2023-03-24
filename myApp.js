d7ca55053a66f9693b6770c198d01433cf935823
Rogério Santos <97257744+rrbooon@users.noreply.github.com>
2023-03-24 16:06:41 +0000

    feat: return a message respecting style in MESSAGE_STYLE

--- myApp.js
+++ myApp.js
@@ -6,10 +6,21 @@ const publicPath = __dirname + '/public';
 
 app.use('/public', express.static(publicPath));
 
-app.get('/json', function (req, res){
-  res.json({
-    "message": "Hello json"
-  })
+app.get('/json', function(req, res) {
+  let message = {}
+  const style = process.env['MESSAGE_STYLE']
+
+  if (style === 'uppercase') {
+    message = {
+      "message": "HELLO JSON"
+    }
+  } else {
+    message = {
+      "message": "Hello json"
+    }
+  }
+
+  res.json(message)
 });
 
 app.get('/', function(req, res) {
